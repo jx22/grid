e5bb3fe0de6385e3afefd8fdf73029cf6dda4867
jx22 <jxpost22@yahoo.com>
2021-04-03 02:01:44 -0700

    add link logic

--- db.js
+++ db.js
@@ -4,20 +4,23 @@ const form = document.querySelector('#add-place-form');
 // create element and render place
 function renderplace(doc){
     let li = document.createElement('li');
-    let description = document.createElement('span');
     let destination = document.createElement('span');
+    let description = document.createElement('span');
     let location = document.createElement('span');
+    let link = document.createElement('span');
     let cross = document.createElement('div');
 
     li.setAttribute('data-id', doc.id)
-    description.textContent = doc.data().description;
     destination.textContent = doc.data().destination;
+    description.textContent = doc.data().description;
     location.textContent = doc.data().location;
+    link.textContent = doc.data().link;
     cross.textContent = 'Delete Entry';
 
-    li.appendChild(description);
     li.appendChild(destination);
+    li.appendChild(description);
     li.appendChild(location);
+    li.appendChild(link);
     li.appendChild(cross);
 
     placeslist.appendChild(li);
@@ -34,13 +37,15 @@ function renderplace(doc){
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     db.collection('places').add({
-        description: form.description.value,
         destination: form.destination.value,
-        location: form.location.value
+        description: form.description.value,
+        location: form.location.value,
+        link: form.link.value
     });
-    form.description.value = '';
     form.destination.value = '';
+    form.description.value = '';
     form.location.value = '';
+    form.link.value = '';
 
 });
 
